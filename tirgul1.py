899a4ce9607b21be1981a534740d964e3a06fb1c
EyabKabha <38509837+EyabKabha@users.noreply.github.com>
2019-03-23 12:56:33 +0200

    python training

---
+++ tirgul1.py
@@ -0,0 +1,13 @@
+def spy_game(nums): #define the function 
+    code = [0,0,7,'x'] # list to check
+    for num in nums:  #loop runs to all numbers in the list
+        # [0,7,'x']
+        # [7,'x']
+        # ['x']  Length = 1 
+        if num == code[0]: 
+            code.pop(0)
+    return len(code) == 1
+
+print(spy_game([1,2,4,0,0,7,5])) # True 
+print(spy_game([1,0,2,4,0,5,7])) # True 
+print(spy_game([1,7,2,0,4,5,0])) # False 
